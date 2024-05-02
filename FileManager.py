2945003c185fc339fd83fc9d401cbc9e7eab9afd
RedWn <redwaniscool@gmail.com>
2024-05-02 12:50:56 +0300

    Update FileManager.py

--- FileManager.py
+++ FileManager.py
@@ -12,3 +12,11 @@ def openCSVWriter(pathToFile, fieldnames):
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     return writer
+
+
+def csvToDict(filename):
+    with open(filename, mode="r") as infile:
+        reader = csv.reader(infile)
+        next(reader, None)  # skip the headers
+        dict_from_csv = {rows[0]: rows[1] for rows in reader}
+    return dict_from_csv
