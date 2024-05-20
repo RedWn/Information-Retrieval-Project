4e098589d7d8630a42d2d8a146ee53a4d18bc587
Iyad Alanssary <iyadalanssary@outlook.com>
2024-05-21 02:19:14 +0300

    Fix Word Embedding

--- python/Matcher.py
+++ python/Matcher.py
@@ -1,5 +1,6 @@
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+from scipy import sparse
 
 
 def calculate_cos_similarity(tfidf_dataframe, vector):
@@ -19,10 +20,25 @@ def calculate_cos_similarity(tfidf_dataframe, vector):
     return related_docs
 
 
-def get_query_answers(corpus_matrix, query_matrix, keys, threshold=0.25):
-    similarity_matrix = cosine_similarity(corpus_matrix, query_matrix)
-    similar_rows_indices = np.where(similarity_matrix > threshold)[0]
+# def get_query_answers(corpus_matrix, query_matrix, keys, threshold=0.25):
+#     similarity_matrix = cosine_similarity(corpus_matrix, query_matrix)
+#     similar_rows_indices = np.where(similarity_matrix > threshold)[0]
 
-    # Get the row names (index) from the DataFrame
-    similar_rows = {keys[i]: similarity_matrix[i].max() for i in similar_rows_indices}
+#     # Get the row names (index) from the DataFrame
+#     similar_rows = {keys[i]: similarity_matrix[i].max() for i in similar_rows_indices}
+#     return similar_rows
+
+
+def get_query_answers(corpus_matrix, query_matrix, keys, threshold=0.25):
+    # Convert the matrices to sparse CSR format
+    query_matrix_sparse = sparse.csr_matrix(query_matrix)
+
+    # Compute cosine similarity
+    similarity_matrix = cosine_similarity(corpus_matrix, query_matrix_sparse, dense_output=False)
+
+    # Convert similarity_matrix to a dense format if necessary
+    similarity_matrix_dense = similarity_matrix.toarray()
+
+    similar_rows_indices = np.where(similarity_matrix_dense > threshold)[0]
+    similar_rows = {keys[i]: similarity_matrix_dense[i].max() for i in similar_rows_indices}
     return similar_rows
