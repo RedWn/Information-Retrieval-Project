6ff6fb0f758de347f8e1854aebfd3c9016a3484b
RedWn <redwaniscool@gmail.com>
2024-05-21 11:38:56 +0300

    Remove dynamic measures

--- python/Evaluater.py
+++ python/Evaluater.py
@@ -3,13 +3,10 @@ from ir_measures import *
 import pandas as pd
 
 
-def evaluate(qrel_path, run_path, parameters):
-    # TODO: convert [nDCG @ 10, P @ 5, P(rel=2) @ 5, Judged @ 10] to parameters
+def evaluate(qrel_path, run_path):
     qrels = ir_measures.read_trec_qrels(qrel_path)
     run = ir_measures.read_trec_run(run_path)
 
-    ans = ir_measures.calc_aggregate(
-        [RR, P @ 10, R @ 10, AP], qrels, run
-    )
+    ans = ir_measures.calc_aggregate([RR, P @ 10, R @ 10, AP], qrels, run)
 
     return ans
