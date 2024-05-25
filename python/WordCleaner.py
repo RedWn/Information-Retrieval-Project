68996d9282d186e5092b96f1abcfa465ceab3954
Iyad Alanssary <iyadalanssary@outlook.com>
2024-05-25 23:29:21 +0300

    Unify numeric words and roman numerals

--- python/WordCleaner.py
+++ python/WordCleaner.py
@@ -6,7 +6,13 @@ import nltk.stem as ns
 from word2number import w2n
 from functools import lru_cache
 import string
+import re
+import spacy
+from number_parser import parse_ordinal
 
+
+# Load the spaCy model
+nlp = spacy.load('en_core_web_sm')
 
 def stem(words, mode):
     stemmer = ns.PorterStemmer()
@@ -23,7 +29,6 @@ def stem(words, mode):
 def get_wordnet_pos(tag_parameter):
     tag = tag_parameter[0].upper()
     tag_dict = {
-        # redwan should we expand this list to take more options TODO
         "J": wordnet.ADJ,
         "N": wordnet.NOUN,
         "V": wordnet.VERB,
@@ -59,68 +64,75 @@ def process_capital_punctuation(words):
     return new_tokens
 
 
-def get_alternative(word):
-    treebank_pos = pos_tag([word])[0][1]  # Get the POS tag
-    synsets = wordnet.synsets(word, pos=get_wordnet_pos(treebank_pos))
-    # synsets = wordnet.synsets(word)
-    if synsets:
-        return synsets[0].lemma_names()[0].lower()  # Choose the first synonym
-    else:
-        return word  # If no synonyms found, keep the original word
+# # Cache the synsets to avoid redundant processing
+# @lru_cache(maxsize=None)
+# def get_synsets(word):
+#     return wordnet.synsets(word)
 
 
-def get_alternative_hypernym(word):
-    synsets = wordnet.synsets(word)
-    if synsets:
-        # Get the first synset (sense)
-        first_synset = synsets[0]
-        # Get the hypernyms for the first synset
-        hypernyms = first_synset.hypernyms()
-        if hypernyms:
-            # Choose the first hypernym
-            return hypernyms[0].lemma_names()[0]
-        else:
-            # If no hypernyms found, return the original word
-            return word
-    else:
-        # If no synsets found, keep the original word
-        return word
+# def get_unified_synonym(word):
+#     # If the word is a digit, return it as is
+#     if word.isdigit():
+#         return str(word)
+
+#     # If the word is a numeric word, return it as a number
+#     try:
+#         return str(w2n.word_to_num(word))
+#     except ValueError:
+#         # Get the synsets once and reuse
+#         synsets = get_synsets(word)
+#         if synsets:
+#             # Directly access lemma names and count occurrences
+#             lemma_names = [
+#                 lemma.name() for synset in synsets for lemma in synset.lemmas()
+#             ]
+#             # Get the most common synonym for the word
+#             unified_synonym = max(set(lemma_names), key=lemma_names.count)
+#             try:
+#                 return str(w2n.word_to_num(unified_synonym))
+#             except ValueError:
+#                 return unified_synonym.lower()
+#     return word.lower()
 
 
-def synonym_map_corpus(corpus):
-    # Update the dataset with synonyms words
-    mapped_dataset = {}
-    for key, words in corpus.items():
-        mapped_dataset[key] = [get_unified_synonym(word) for word in words]
-    return mapped_dataset
+# Create a dictionary to store precomputed results
+memo = {}
+def get_unified_synonym_2(word):
+    # Check if the result is already in the dictionary
+    if word in memo:
+        return memo[word]
 
-
-# Cache the synsets to avoid redundant processing
-@lru_cache(maxsize=None)
-def get_synsets(word):
-    return wordnet.synsets(word)
-
-
-def get_unified_synonym(word):
-    # If the word is a digit, return it as is
+    # Otherwise, compute the result as before
     if word.isdigit():
-        return str(word)
+        result = str(word)
+    
+    # Convert Roman numeral to integer
+    elif is_roman_numeral(word):
+        result = str(roman.fromRoman(word.upper()))
+    
+    # Convert ordianl words like "first" to "1st"
+    elif is_ordinal(word):
+        result = ordinal_word_to_ordinal_number(word.lower())
 
+    else:
     # If the word is a numeric word, return it as a number
-    try:
-        return str(w2n.word_to_num(word))
-    except ValueError:
-        # Get the synsets once and reuse
-        synsets = get_synsets(word)
-        if synsets:
-            # Directly access lemma names and count occurrences
-            lemma_names = [
-                lemma.name() for synset in synsets for lemma in synset.lemmas()
-            ]
-            # Get the most common synonym for the word
-            unified_synonym = max(set(lemma_names), key=lemma_names.count)
-            try:
-                return str(w2n.word_to_num(unified_synonym))
-            except ValueError:
-                return unified_synonym.lower()
-    return word.lower()
+        try:
+            result = str(w2n.word_to_num(word))
+        except ValueError:    
+            result = word.lower()
+    
+    result = standardize_country_names(result)
+    # Store the result in the dictionary
+    memo[word] = result
+    return result
+
+
+
+
+
+# This pattern matches Roman numerals from 1 to 49
+pattern = '(I|II|III|IV|V|VI|VII|VIII|IX|X|XX|XXX|XL|L)?'
+def is_roman_numeral(s):
+    return bool(re.fullmatch(pattern, s, re.IGNORECASE))
+
+
