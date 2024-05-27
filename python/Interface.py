20a6d2435acb48d3571bf5b12c3921ed87b444a6
RedWn <redwaniscool@gmail.com>
2024-05-28 01:37:05 +0300

    Prepare UI for integration

--- python/Interface.py
+++ python/Interface.py
@@ -1,16 +1,18 @@
-from python import FileManager
-from python import WordCleaner
-from python import Indexer
-from python import Matcher
+import FileManager
+import WordCleaner
+import Indexer
+import Matcher
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import word_tokenize
 
 
-def search(dataset, query):
+def search(dataset_name: str, query: str):
     query = word_tokenize(query)
     query = WordCleaner.remove_stop_words(query)
     query = WordCleaner.lemmatize(query)
-    vectorizer, dataset_keys, tfidf_matrix = FileManager.load_model_from_drive(dataset)
+    vectorizer, dataset_keys, tfidf_matrix = FileManager.load_model_from_drive(
+        "../" + dataset_name
+    )
     matrix = Indexer.calculate_doc_tf_idf([" ".join(query)], vectorizer)
     similar_rows = Matcher.get_query_answers(tfidf_matrix, matrix, dataset_keys, 0.35)
     return similar_rows
