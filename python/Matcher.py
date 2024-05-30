52b478698cc50409f54478ab799d7d906976848f
Iyad Alanssary <iyadalanssary@outlook.com>
2024-05-30 21:32:05 +0300

    Update Matcher

--- python/Matcher.py
+++ python/Matcher.py
@@ -4,35 +4,18 @@ import numpy as np
 from scipy import sparse
 
 
-def get_query_answers(corpus_matrix, query_matrix, keys, threshold=0.25):
-    query_matrix_sparse = sparse.csr_matrix(query_matrix)
+def get_query_answers(corpus_matrix, query_matrix,keys, threshold=0.25):
+    # query_matrix_sparse = sparse.csr_matrix(query_matrix)
 
-    similarity_matrix = cosine_similarity(corpus_matrix, query_matrix_sparse)
+    similarity_matrix = cosine_similarity(corpus_matrix, query_matrix).reshape(-1)
     similar_rows_indices = np.where(similarity_matrix > threshold)[0]
 
     # Precompute max values for each row
-    max_values = similarity_matrix.max(axis=1)
+    # max_values = similarity_matrix.max(axis=1)
 
     # Use a generator expression instead of a dictionary comprehension
-    similar_rows = ((keys[i], max_values[i]) for i in similar_rows_indices)
-    similar_rows = dict(similar_rows)
-    similar_rows = {
-        k: v
-        for k, v in sorted(similar_rows.items(), key=lambda item: item[1], reverse=True)
-    }
-    return similar_rows
-
-
-def get_query_answers_optimized_bert(corpus_matrix, query_matrix, keys, threshold=0.25):
-    similarity_matrix = cosine_similarity(corpus_matrix, query_matrix)
-    similar_rows_indices = np.where(similarity_matrix > threshold)[0]
-
-    # Precompute max values for each row
-    max_values = similarity_matrix.max(axis=1)
-
-    # Use a generator expression instead of a dictionary comprehension
-    similar_rows = ((keys[i], max_values[i]) for i in similar_rows_indices)
-    similar_rows = dict(similar_rows)
+    similar_rows = {keys[i]: similarity_matrix[i] for i in similar_rows_indices}
+    # similar_rows = dict(similar_rows)
     similar_rows = {
         k: v
         for k, v in sorted(similar_rows.items(), key=lambda item: item[1], reverse=True)
