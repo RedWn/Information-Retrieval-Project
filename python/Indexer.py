1fa53c52fb9887565c6168cd70b6d671780d8a7f
RedWn <redwaniscool@gmail.com>
2024-06-02 18:22:31 +0300

    Migrate word2vec embed to indexer

--- python/Indexer.py
+++ python/Indexer.py
@@ -1,4 +1,5 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 
 
 def calculate_doc_tf_idf(query, vectorizer: TfidfVectorizer):
@@ -7,6 +8,14 @@ def calculate_doc_tf_idf(query, vectorizer: TfidfVectorizer):
 
 def calculate_doc_embedding(query, model):
     return model.encode(query).reshape(1, -1)
+
+
+def calculate_doc_vector(query, model):
+    valid_vectors = [model.wv[word] for word in query if word in model.wv]
+    if valid_vectors:
+        query_vector = np.mean(valid_vectors, axis=0).reshape(1, -1)
+        return query_vector
+    return np.ndarray()
 
 
 def calculate_tf_idf(corpus, vectorizer: TfidfVectorizer):
--- python/app.py
+++ python/app.py
@@ -38,14 +38,16 @@ if (
 
 def select_dataset():
     st.session_state["query"] = None
-    st.write(st.session_state["dataset"])
     if st.session_state["dataset"] == "wikir":
         model_string = "model/lotte"
+        st.session_state["model"] = SentenceTransformer(
+            "sentence-transformers/all-MiniLM-L6-v2"
+        )
     else:
         model_string = "model/wikir"
-    st.session_state["model"] = SentenceTransformer(
-        "sentence-transformers/all-MiniLM-L6-v2"
-    )  # TODO switch to word2vec
+        (st.session_state["model"],) = FileManager.load_word2vec_model(
+            "model/wikir/embedding_8_epoch_20.model"
+        )
     (
         st.session_state["vectorizer"],
         st.session_state["dataset_keys"],
@@ -58,56 +60,78 @@ def select_mode():
     st.session_state["query"] = None
 
 
-st.session_state["dataset"] = st.selectbox(
-    "Dataset",
-    ["wikir", "lotte"],
-    help="Choose the dataset to search",
-    disabled=st.session_state.disabled,
-    label_visibility=st.session_state.visibility,
-    on_change=select_dataset,
-)
+col1, col2 = st.columns(2)
 
-st.session_state["mode"] = st.selectbox(
-    "Indexing Method",
-    ["tf-idf", "embedding"],
-    help="choose whether to use TF-IDF or Embedding",
-    disabled=st.session_state.disabled,
-    label_visibility=st.session_state.visibility,
-    on_change=select_mode,
-)
+with col1:
+    st.session_state["dataset"] = st.selectbox(
+        "Dataset",
+        ["wikir", "lotte"],
+        help="Choose the dataset to search",
+        disabled=st.session_state.disabled,
+        label_visibility=st.session_state.visibility,
+        on_change=select_dataset,
+    )
 
-st.session_state["query"] = st.text_input(
-    "Enter your query:",
-    label_visibility=st.session_state.visibility,
-    disabled=st.session_state.disabled,
-)
+    st.session_state["mode"] = st.selectbox(
+        "Indexing Method",
+        ["tf-idf", "embedding"],
+        help="choose whether to use TF-IDF or Embedding",
+        disabled=st.session_state.disabled,
+        label_visibility=st.session_state.visibility,
+        on_change=select_mode,
+    )
 
-if st.session_state["query"]:
-    query = WordCleaner.query_cleaning(st.session_state["query"])
-    if st.session_state["mode"] == "tf-idf":
-        answers = Matcher.get_query_answers(
-            st.session_state["sparse_matrix"],
-            Indexer.calculate_doc_tf_idf(
-                [" ".join(query)], st.session_state["vectorizer"]
-            ),
-            st.session_state["dataset_keys"],
-            0.35,
-        )
-    else:
-        answers = Matcher.get_query_answers(
-            st.session_state["matrix"],
-            Indexer.calculate_doc_embedding(" ".join(query), st.session_state["model"]),
-            st.session_state["dataset_keys"],
-            0.35,
-        )
-    st.write("You entered: ", st.session_state["query"])
-    col1, col2 = st.columns([4, 1])
-    if len(list(answers.keys())) > 0:
-        text = Interface.get_rows_by_ids(
-            st.session_state["dataset"], list(answers.keys())[:10]
-        )
-    for i, answer in enumerate(list(answers.keys())[:10]):
-        col1.container = st.container(border=True)
-        col1.container.header(" ".join(word_tokenize(text[i][1])[0:3]))
-        col1.container.write(text[i][1])
-        col1.container.caption(":orange[ Result ID #{}]".format(answer))
+    st.session_state["query"] = st.text_input(
+        "Enter your query:",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+    )
+
+    if st.session_state["query"]:
+        query = WordCleaner.query_cleaning(st.session_state["query"])
+        if st.session_state["mode"] == "tf-idf":
+            answers = Matcher.get_query_answers(
+                st.session_state["sparse_matrix"],
+                Indexer.calculate_doc_tf_idf(
+                    [" ".join(query)], st.session_state["vectorizer"]
+                ),
+                st.session_state["dataset_keys"],
+                0.35,
+            )
+        else:
+            if st.session_state["query"] == "wikir":
+                answers = Matcher.get_query_answers(
+                    st.session_state["matrix"],
+                    Indexer.calculate_doc_vector(
+                        " ".join(query), st.session_state["model"]
+                    ),
+                    st.session_state["dataset_keys"],
+                    0.35,
+                )
+            else:
+                answers = Matcher.get_query_answers(
+                    st.session_state["matrix"],
+                    Indexer.calculate_doc_embedding(
+                        " ".join(query), st.session_state["model"]
+                    ),
+                    st.session_state["dataset_keys"],
+                    0.35,
+                )
+        st.write("You entered: ", st.session_state["query"])
+        subcol1, subcol2 = st.columns([4, 1])
+        if len(list(answers.keys())) > 0:
+            text = Interface.get_rows_by_ids(
+                st.session_state["dataset"], list(answers.keys())[:10]
+            )
+        for i, answer in enumerate(list(answers.keys())[:10]):
+            subcol1.container = st.container(border=True)
+            subcol1.container.header(" ".join(word_tokenize(text[i][1])[0:3]))
+            subcol1.container.write(text[i][1])
+            subcol1.container.caption(":orange[ Result ID #{}]".format(answer))
+
+with col2:
+    st.radio(
+        "Set selectbox label visibility 👉",
+        key="visibility",
+        options=["visible", "hidden", "collapsed"],
+    )
