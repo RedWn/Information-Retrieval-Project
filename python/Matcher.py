baa9d5096729b7c546b48c8db1afff1b26824377
Iyad Alanssary <iyadalanssary@outlook.com>
2024-06-02 17:22:10 +0300

    Encapsulate word2vec manual query

--- python/Matcher.py
+++ python/Matcher.py
@@ -1,7 +1,6 @@
 from sklearn.metrics.pairwise import cosine_similarity
-from scipy.sparse import csr_matrix
 import numpy as np
-from scipy import sparse
+from python import WordCleaner
 
 
 def get_query_answers(corpus_matrix, query_matrix, keys, threshold=0.5):
@@ -16,3 +15,23 @@ def get_query_answers(corpus_matrix, query_matrix, keys, threshold=0.5):
         for k, v in sorted(similar_rows.items(), key=lambda item: item[1], reverse=True)
     }
     return similar_rows
+
+
+
+def get_word2vec_ans(query, model, documents_vectors, dataset_keys):
+
+    query = WordCleaner.query_cleaning(query)
+    print(query)
+
+    valid_vectors = [model.wv[word] for word in query if word in model.wv]
+
+    # Check if there are valid vectors to avoid nan issues
+    if valid_vectors:
+        query_vector = np.mean(valid_vectors, axis=0).reshape(1, -1)
+        # Compute cosine similarity between query and document vectors
+        similar_docs = get_query_answers(documents_vectors, query_vector, dataset_keys, 0.55)
+        # Print the IDs of the top 5 most similar documents
+        for i, (doc_id, score) in enumerate(list(similar_docs.items())[:10]):
+            print(f"Rank {i+1}, Document ID: {doc_id}, Similarity Score: {score}")
+    else:
+        print("None of the query words were found in the model's vocabulary.")
